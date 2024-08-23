2adc6b79213dca208781d19174855b6bf42d5615
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-08-23 16:33:40 -0400

    chore: update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'f0c3fdffe38dca01c158');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '76111af598df3acd1482');
