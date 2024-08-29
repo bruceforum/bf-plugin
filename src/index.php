0ade3b4ae974ec64c3d7154f28259976aa3a9028
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-08-29 19:32:40 -0400

    fix: update input rendering

--- src/index.php
+++ src/index.php
@@ -7,32 +7,33 @@
 <?php
 function render_category_search_input($attributes)
 {
-	$input_id = wp_unique_id('wp-block-search__input-');
+	// $input_id = wp_unique_id('wp-block-search__input-');
 
-	$label_inner_html = empty($attributes['label']) ? __('Search') : wp_kses_post($attributes['label']);
-	$label = new WP_HTML_Tag_Processor(sprintf('<label>%1$s</label>', $label_inner_html));
-	if ($label->next_tag()) {
-		$label->set_attribute('for', $input_id);
-		$label->add_class('wp-block-search__label');
-	}
+	// $label_inner_html = empty($attributes['label']) ? __('Search') : wp_kses_post($attributes['label']);
+	// $label = new WP_HTML_Tag_Processor(sprintf('<label>%1$s</label>', $label_inner_html));
+	// if ($label->next_tag()) {
+	// 	$label->set_attribute('for', $input_id);
+	// 	$label->add_class('wp-block-search__label');
+	// }
 
-	$input = new WP_HTML_Tag_Processor(sprintf('<input type="search" name="qls" required />'));
-	if ($input->next_tag()) {
-		$input->set_attribute('id', $input_id);
-		$input->set_attribute('value', get_search_query());
-		$input->set_attribute('placeholder', $attributes['placeholder']);
-	}
+	// $input = new WP_HTML_Tag_Processor(sprintf('<input type="search" name="qls" required />'));
+	// if ($input->next_tag()) {
+	// 	$input->set_attribute('id', $input_id);
+	// 	$input->set_attribute('value', get_search_query());
+	// 	$input->set_attribute('placeholder', $attributes['placeholder']);
+	// }
 
-	$field_markup = sprintf(
-		'<div class="wp-block-search__inside-wrapper %s" %s>%s</div>',
-		$input
-	);
+	// $field_markup = sprintf(
+	// 	'<div class="wp-block-search__inside-wrapper %s" %s>%s</div>',
+	// 	$input
+	// );
 
-	return sprintf(
-		'<form role="search" method="get" action="%1s">%2s</form>',
-		esc_url(home_url('/')),
-		$label . $field_markup
-	);
+	// return sprintf(
+	// 	'<form role="search" method="get" action="%1s">%2s</form>',
+	// 	esc_url(home_url('/')),
+	// 	$label . $field_markup
+	// );
+	return '<div></div>';
 }
 
 function render_category_search_debug()
@@ -42,6 +43,10 @@ function render_category_search_debug()
 
 function render_category_search($attributes)
 {
-	return '<div></div>';
+	return sprintf(
+		'<div>%1s %2s</div>',
+		render_category_search_input($attributes),
+		render_category_search_debug()
+	);
 }
 ?>
