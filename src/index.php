851197e34ff5a3368c4653d30e520902dcda117f
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-08-29 19:45:50 -0400

    feat: complete render script

--- src/index.php
+++ src/index.php
@@ -8,32 +8,31 @@
 function render_category_search_input($attributes)
 {
 	$input_id = wp_unique_id('wp-block-search__input-');
-	
+
 	$label_inner_html = empty($attributes['label']) ? __('Search') : wp_kses_post($attributes['label']);
 	$label = new WP_HTML_Tag_Processor(sprintf('<label>%1$s</label>', $label_inner_html));
 	if ($label->next_tag()) {
 		$label->set_attribute('for', $input_id);
 		$label->add_class('wp-block-search__label');
 	}
-	
+
 	$input = new WP_HTML_Tag_Processor('<input type="search" name="qls" required />');
 	if ($input->next_tag()) {
 		$input->set_attribute('id', $input_id);
 		$input->set_attribute('value', get_search_query('qls'));
 		$input->set_attribute('placeholder', $attributes['placeholder']);
 	}
-	return '<div></div>';
-		
-	// $field_markup = sprintf(
-	// 	'<div class="wp-block-search__inside-wrapper %s" %s>%s</div>',
-	// 	$input
-	// );
 
-	// return sprintf(
-	// 	'<form role="search" method="get" action="%1$s">%2$s</form>',
-	// 	esc_url(home_url('/')),
-	// 	$label . $field_markup
-	// );
+	$field_markup = sprintf(
+		'<div class="wp-block-search__inside-wrapper">%s</div>',
+		$input
+	);
+
+	return sprintf(
+		'<form role="search" method="get" action="%1$s">%2$s</form>',
+		esc_url(home_url('/')),
+		$label . $field_markup
+	);
 }
 
 function render_category_search_debug($attributes)
