1ee6b5e7f9d8c7240587470a1cbc62a447b5d02c
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-08-30 13:03:34 -0400

    refactor: split functionality

--- build/index.php
+++ build/index.php
@@ -5,45 +5,8 @@
  */
 ?>
 <?php
-function create_hidden_input($name, $value)
+function create_category_input($attributes)
 {
-	$input = new WP_HTML_Tag_Processor(sprintf('<input type="hidden" name="%1$s" required />', $name));
-	if ($input->next_tag()) {
-		$input->set_attribute('value', $value);
-	}
-
-	return $input;
-}
-
-function create_input_for($label, $placeholder, $type, $name, $value)
-{
-	$input_id = wp_unique_id('wp-block-search__input-');
-
-	$label_inner_html = empty($attributes['label']) ? __('Search') : $label;
-	$label = new WP_HTML_Tag_Processor(sprintf('<label>%1$s</label>', $label_inner_html));
-	if ($label->next_tag()) {
-		$label->set_attribute('for', $input_id);
-		$label->add_class('wp-block-search__label');
-	}
-
-	$input = new WP_HTML_Tag_Processor(sprintf('<input type="%1$s" name="%2$s" required />', $type, $name));
-	if ($input->next_tag()) {
-		$input->set_attribute('id', $input_id);
-		$input->set_attribute('value', $value);
-		$input->set_attribute('placeholder', $placeholder);
-	}
-	
-	$field_markup = sprintf(
-		'<div class="wp-block-search__inside-wrapper">%s</div>',
-		$input
-	);
-
-	return $label . $field_markup;
-}
-
-function render_category_search_input($attributes)
-{
-	global $wp;
 	$categories = ''; 
 
 	if (!empty($attributes['categories'])) {
@@ -55,10 +18,51 @@ function render_category_search_input($attributes)
 		}, $categories);
 	}
 
-	$submit_button = '<button type="submit" class="wp-block-button wp-element-button">Search</button>';
-	$search_input = create_input_for(wp_kses_post($attributes['label']), $attributes['placeholder'], 'search', 'qls', get_query_var('qls'));
-	$category_input = create_hidden_input('qlcat', $categories);
+	$input = new WP_HTML_Tag_Processor('<input type="hidden" name="qlcat" required />');
+	if ($input->next_tag()) {
+		$input->set_attribute('value', $categories);
+	}
 
+	return $input;
+}
+
+function create_search_input($attributes)
+{
+	$input_id = wp_unique_id('wp-block-search__input-');
+
+	$label_inner_html = empty($attributes['label']) ? __('Search') : $attributes['label'];
+	$label = new WP_HTML_Tag_Processor(sprintf('<label>%1$s</label>', $label_inner_html));
+	if ($label->next_tag()) {
+		$label->set_attribute('for', $input_id);
+		$label->add_class('wp-block-search__label');
+	}
+
+	$input = new WP_HTML_Tag_Processor('<input type="search" name="qls" required />');
+	if ($input->next_tag()) {
+		$input->set_attribute('id', $input_id);
+		$input->set_attribute('value', get_query_var('qls'));
+		$input->set_attribute('placeholder', $attributes['placeholder']);
+	}
+	
+	$field_markup = sprintf(
+		'<div class="wp-block-search__inside-wrapper">%s</div>',
+		$input
+	);
+
+	return $label . $field_markup;
+}
+
+function create_search_button($attributes)
+{
+	$submit_button = '<button type="submit" class="wp-block-button wp-element-button">Search</button>';
+
+	return $submit_button;
+}
+
+function render_category_search_input($attributes)
+{
+	global $wp;
+	
 	return sprintf(
 		'<form role="search" method="get" action="%1$s">
 			<div>
@@ -68,9 +72,9 @@ function render_category_search_input($attributes)
 			%4$s
 		</form>',
 		esc_url(home_url($wp->request)),
-		$search_input,
-		$submit_button,
-		$category_input
+		create_search_input($attributes),
+		create_search_button($attributes),
+		create_category_input($attributes)
 	);
 }
 
