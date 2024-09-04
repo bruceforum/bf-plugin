0889656f1d49f0dda336d9eb464d186476a28090
Michael Wrock <MichaelA.Wrock@gmail.com>
2024-09-04 19:53:21 -0400

    feat: add fields to contact

--- build/index.php
+++ build/index.php
@@ -7,113 +7,113 @@
 <?php
 function get_posts_years_array()
 {
-	global $wpdb;
-	$result = array();
-	$years = $wpdb->get_results(
-		$wpdb->prepare(
-			"SELECT YEAR(post_date) FROM {$wpdb->posts} WHERE post_status = 'publish' GROUP BY YEAR(post_date) DESC"
-		),
-		ARRAY_N
-	);
-	if (is_array($years) && count($years) > 0) {
-		foreach ($years as $year) {
-			$result[] = strval($year[0]);
-		}
-	}
-	return $result;
+    global $wpdb;
+    $result = array();
+    $years = $wpdb->get_results(
+        $wpdb->prepare(
+            "SELECT YEAR(post_date) FROM {$wpdb->posts} WHERE post_status = 'publish' GROUP BY YEAR(post_date) DESC"
+        ),
+        ARRAY_N
+    );
+    if (is_array($years) && count($years) > 0) {
+        foreach ($years as $year) {
+            $result[] = strval($year[0]);
+        }
+    }
+    return $result;
 }
 
 function wrap_input($label, $input, $class)
 {
-	$input_id = wp_unique_id('wp-block-search__input-');
+    $input_id = wp_unique_id('wp-block-search__input-');
 
-	$label_inner_html = empty($label) ? __('Search') : $label;
-	$label = new WP_HTML_Tag_Processor(sprintf('<label>%s</label>', $label_inner_html));
-	if ($label->next_tag()) {
-		$label->set_attribute('for', $input_id);
-		$label->add_class('wp-block-search__label');
-	}
+    $label_inner_html = empty($label) ? __('Search') : $label;
+    $label = new WP_HTML_Tag_Processor(sprintf('<label>%s</label>', $label_inner_html));
+    if ($label->next_tag()) {
+        $label->set_attribute('for', $input_id);
+        $label->add_class('wp-block-search__label');
+    }
 
-	$input->set_attribute('id', $input_id);
+    $input->set_attribute('id', $input_id);
 
-	$field_markup = sprintf(
-		'<div class="wp-block-search__inside-wrapper %s">%s</div>',
-		$class,
-		$input
-	);
+    $field_markup = sprintf(
+        '<div class="wp-block-search__inside-wrapper %s">%s</div>',
+        $class,
+        $input
+    );
 
-	return sprintf(
-		'<div>%s %s</div>',
-		$label,
-		$field_markup
-	);
+    return sprintf(
+        '<div>%s %s</div>',
+        $label,
+        $field_markup
+    );
 }
 
 function create_input_for($placeholder, $type, $name, $value)
 {
-	$input = new WP_HTML_Tag_Processor(sprintf('<input type="%s" name="%s" required />', $type, $name));
-	if ($input->next_tag()) {
-		$input->set_attribute('value', $value);
-		$input->set_attribute('placeholder', $placeholder);
-	}
+    $input = new WP_HTML_Tag_Processor(sprintf('<input type="%s" name="%s" required />', $type, $name));
+    if ($input->next_tag()) {
+        $input->set_attribute('value', $value);
+        $input->set_attribute('placeholder', $placeholder);
+    }
 
-	return $input;
+    return $input;
 }
 
 function create_select_for($name, $value, $options)
 {
-	$options_html = array_reduce($options, function ($ax, $dx) use ($value) {
-		if ($value === $dx) {
-			return $ax . sprintf('<option selected>%s</option>', $dx);
-		}
-		return $ax . sprintf('<option>%s</option>', $dx);
-	}, '');
+    $options_html = array_reduce($options, function ($ax, $dx) use ($value) {
+        if ($value === $dx) {
+            return $ax . sprintf('<option selected>%s</option>', $dx);
+        }
+        return $ax . sprintf('<option>%s</option>', $dx);
+    }, '');
 
-	$select = new WP_HTML_Tag_Processor(sprintf('<select name="%s" required>%s</select>', $name, $options_html));
+    $select = new WP_HTML_Tag_Processor(sprintf('<select name="%s" required>%s</select>', $name, $options_html));
 
-	return $select;
+    return $select;
 }
 
 function create_category_input($attributes)
 {
-	$categories = '';
+    $categories = '';
 
-	if (!empty($attributes['categories'])) {
-		$categories = array_reduce($attributes['categories'], function ($ax, $dx) {
-			if (empty($ax)) {
-				return $ax . (int)$dx['id'];
-			}
-			return $ax . ',' . (int)$dx['id'];
-		}, $categories);
-	}
+    if (!empty($attributes['categories'])) {
+        $categories = array_reduce($attributes['categories'], function ($ax, $dx) {
+            if (empty($ax)) {
+                return $ax . (int)$dx['id'];
+            }
+            return $ax . ',' . (int)$dx['id'];
+        }, $categories);
+    }
 
-	$input = new WP_HTML_Tag_Processor('<input type="hidden" name="qlcat" required />');
-	if ($input->next_tag()) {
-		$input->set_attribute('value', $categories);
-	}
+    $input = new WP_HTML_Tag_Processor('<input type="hidden" name="qlcat" required />');
+    if ($input->next_tag()) {
+        $input->set_attribute('value', $categories);
+    }
 
-	return $input;
+    return $input;
 }
 
 function create_search_button($attributes)
 {
-	$submit_button = '<button type="submit" class="wp-block-button wp-element-button">Search</button>';
+    $submit_button = '<button type="submit" class="wp-block-button wp-element-button">Search</button>';
 
-	return $submit_button;
+    return $submit_button;
 }
 
-function render_category_search_input($attributes)
+function render_category_search($attributes)
 {
-	global $wp;
+    global $wp;
 
-	$year_options = array_merge(['all'], get_posts_years_array());
+    $year_options = array_merge(['all'], get_posts_years_array());
 
-	$search_input = create_input_for($attributes['placeholder'], 'search', 'qls', get_query_var('qls'));
-	$year_select = create_select_for('qlyear', get_query_var('qlyear', 'all'), $year_options);
-	$sort_select = create_select_for('qlorderby', get_query_var('qlorderby', 'date'), ['date', 'title', 'relevance']);
+    $search_input = create_input_for($attributes['placeholder'], 'search', 'qls', get_query_var('qls'));
+    $year_select = create_select_for('qlyear', get_query_var('qlyear', 'all'), $year_options);
+    $sort_select = create_select_for('qlorderby', get_query_var('qlorderby', 'date'), ['date', 'title', 'relevance']);
 
-	return sprintf(
-		'<form role="search" method="get" action="%1$s">
+    return sprintf(
+        '<form role="search" method="get" action="%1$s">
 			%2$s
 			<div class="bb-search-wrapper">
 				%3$s
@@ -124,25 +124,39 @@ function render_category_search_input($attributes)
 				%6$s
 			</div>
 		</form>',
-		esc_url(home_url($wp->request)),
-		create_category_input($attributes),
-		wrap_input($attributes['label'], $search_input, 'bb-search'),
-		create_search_button($attributes),
-		wrap_input('Year', $year_select, 'bb-year'),
-		wrap_input('Sort by', $sort_select, 'bb-sort')
-	);
-}
-
-function render_category_search($attributes)
-{
-	return sprintf(
-		'<div>%1$s</div>',
-		render_category_search_input($attributes)
-	);
+        esc_url(home_url($wp->request)),
+        create_category_input($attributes),
+        wrap_input($attributes['label'], $search_input, 'bb-search'),
+        create_search_button($attributes),
+        wrap_input('Year', $year_select, 'bb-year'),
+        wrap_input('Sort by', $sort_select, 'bb-sort')
+    );
 }
 
 function render_contact_form($attributes)
 {
-	return '<div>Hello World!</div>';
+    $firstname_input = create_input_for("", "text", 'firstname', "");
+    $lastname_input = create_input_for("", "text", 'lastname', "");
+    $email_input = create_input_for("name@mycompany.com", "email", 'email', "");
+    $phone_input = create_input_for("+1 235 454 6789", "phone", 'phone', "");
+    $summary_input = create_input_for("Ex : I need help with my account.", "text", 'summary', "");
+    $detail_input = create_input_for("Write down all the details here", "text", 'detail', "");
+
+    return sprintf(
+        '<form role="contact">
+            %s
+            %s
+            %s
+            %s
+            %s
+            %s
+		</form>',
+        wrap_input("First Name", $firstname_input, ""),
+        wrap_input("Last Name", $lastname_input, ""),
+        wrap_input("Email", $$email_input, ""),
+        wrap_input("Telephone number", $phone_input, ""),
+        wrap_input("What is your request?", $summary_input, ""),
+        wrap_input("How can we help you?", $detail_input, "")
+    );
 }
 ?>
